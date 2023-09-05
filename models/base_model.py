90fceeff2538754c955dd38c1b4d71ac4bb58d4d
mugisha-blaine <n.blaine@alustudent.com>
2023-09-05 16:18:22 +0200

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,20 @@ format_date = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel:
     """ Basemodel class """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initialization of Database """
-
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if args is not None and len(args) > 0:
+            pass
+        if kwargs:
+            for key, item in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    item = datetime.strptime(item, format_date)
+                if key not in ['__class__']:
+                    setattr(self, key, item)
+        else:
+            self.id = str(uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """str definition"""
