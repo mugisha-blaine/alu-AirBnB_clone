3255c116240780c6a42502b345f5a381573a7701
mugisha-blaine <n.blaine@alustudent.com>
2023-09-05 16:36:20 +0200

    basemodel

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 import unittest
 from models.engine.file_storage import FileStorage
+#import os
+from models.base_model import BaseModel
+
 
 
 class TestFileStorage(unittest.TestCase):
+    ''' Test File storage '''
 
-    def setUp(self):
-        
-        self.storage = FileStorage()
+    def test_pep8_FileStorage(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(p.total_errors, 0, "Check pep8")
 
-    def test_attrs(self):
 
-        self.assertFalse(hasattr(self.storage, "milkyway.json"))
 
-if "__main__" == __name__:
+if __name__ == "__main__":
     unittest.main()
