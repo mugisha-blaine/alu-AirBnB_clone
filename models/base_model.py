1df930d773b3c69b25d3daa7d0b2fc5fcec42b49
mugisha-blaine <n.blaine@alustudent.com>
2023-09-05 15:16:01 +0200

    basemodel

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Module for Base """
+
+from datetime import datetime
+from uuid import uuid4
+import json
+import models
+
+format_date = "%Y-%m-%dT%H:%M:%S.%f"
+
+
+class BaseModel:
+    """ Basemodel class """
+    def __init__(self):
+        """ Initialization of Database """
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """str definition"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """save definition"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
