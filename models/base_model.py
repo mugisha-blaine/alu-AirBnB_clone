57d3bdb198188532262176e2ffedb5b4c3a282d0
mugisha-blaine <n.blaine@alustudent.com>
2023-09-05 15:28:44 +0200

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
     def __init__(self):
         """ Initialization of Database """
 
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
