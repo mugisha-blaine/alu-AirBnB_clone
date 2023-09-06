040937f5da224e3cd0029799dd181c2db7aa5ebe
Sam <s.olubode@alustudent.com>
2023-09-06 09:09:59 +0100

    Add commands

--- console.py
+++ console.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-  AirBnB Console Application
-"""
+"""AirBnB Console Application"""
 
 import cmd
 
@@ -11,21 +9,15 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """
-          Exit CLI when `quit` is entered
-        """
+        """Exit CLI when `quit` is entered"""
         return True
 
     def do_EOF(self, arg):
-        """ 
-          Execute nothing for an EOF input
-        """
+        """Execute nothing for an EOF input"""
         return True
 
     def emptyline(self):
-        """ 
-          Execute nothing for an empty line command
-        """
+        """Execute nothing for an empty line command"""
         pass
 
 
