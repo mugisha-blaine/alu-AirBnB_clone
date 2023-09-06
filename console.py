14384f1bf9734121dc229455947b310c182c659b
Sam <s.olubode@alustudent.com>
2023-09-06 10:37:32 +0100

    update

--- console.py
+++ console.py
@@ -2,10 +2,18 @@
 """AirBnB Console Application"""
 
 import cmd
+from models.engine import name_class
+from models import storage
+from models import city
+from models import place
+from models import review
+from models import state
+from models import amenity
+from models import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
-    """Console APP Subclass
+    """AirBnB Console APP Subclass
     """
     prompt = '(hbnb) '
 
@@ -20,6 +28,19 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Execute nothing for an empty line command"""
         pass
+
+    def do_create(self, args):
+        """
+                Creates a new instance of BaseModel
+                create: create <class_name> (ex: create BaseModel)
+        """
+
+        if len(args) < 1:
+            print("** class name missing **")
+            return
+        if args[0] not in name_class:
+            print("** class doesn't exist **")
+            return
 
 
 if __name__ == "__main__":
