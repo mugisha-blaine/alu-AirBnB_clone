8351bbd539084dc3277e9add85ed1ac5463ef2e6
mugisha-blaine <n.blaine@alustudent.com>
2023-09-12 22:25:13 +0200

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,7 @@ class BaseModel:
 
     def __str__(self):
         """str definition"""
-        return "[{}] ({}) {}".format(self.__class__.__name__,\
+        return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
