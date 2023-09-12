245b705f07a52510158e06897217476dea0f3718
mugisha-blaine <n.blaine@alustudent.com>
2023-09-12 21:40:09 +0200

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,7 @@ class BaseModel:
         else:
             self.id = str(uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
+            #models.storage.new(self)
 
     def __str__(self):
         """str definition"""
