47cd404a1cb060dccdeca10edfdd88abaaedcced
mugisha-blaine <n.blaine@alustudent.com>
2023-09-13 14:07:41 +0200

    basemodel

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-""" Test Place """
-
 import unittest
-from models import Place
-from models.base_model import BaseModel
+from models import City
 
 
-class Test_Place(unittest.TestCase):
-    """ Tests Place """
 
-    def test_Place_dict(self):
-        """ Place_dict """
-        place = Place()
-        self.assertTrue('id' in place.to_dict())
-        self.assertTrue('created_at' in place.to_dict())
-        self.assertTrue('updated_at' in place.to_dict())
-        self.assertTrue('__class__' in place.to_dict())
+class TestCity(unittest.TestCase):
 
-    def test_save_Place(self):
-        """ Save_Place """
-        place = Place()
-        self.assertEqual(place.created_at, place.updated_at)
+    def setUp(self):
+        self.test_model1 = City()
+        self.test_model2 = City()
 
+    def test_basic_setup(self):
+    
+        self.assertTrue(hasattr(self.test_model1, "state_id"))
+        self.assertTrue(hasattr(self.test_model1, "name"))
+        self.assertTrue(self.test_model1.id != self.test_model2.id)
+
+    def test_types(self):
+    
+        self.assertTrue(type(self.test_model1.state_id) is str)
+        self.assertTrue(type(self.test_model1.name) is str)
+
+    def test_save(self):
+    
+        m1u = self.test_model1.updated_at
+        self.test_model1.save()
+        m1u_saved = self.test_model1.updated_at
+        self.assertFalse(m1u == m1u_saved)
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,68 +1,36 @@
 #!/usr/bin/python3
-""" unit test for class State """
-
-from models.state import State
-import os
 import unittest
-
-
-class TestStateDocs(unittest.TestCase):
-    """ Test docstring in the class """
-
-    def test_doc_class(self):
-        """ Test document class """
-        doc = State.__doc__
-        self.assertIsNotNone(doc)
-
-    def test_doc_methods_class(self):
-        """ Test document methods Class """
-        l_method = ["save", "__init__", "__str__", "to_dict"]
-        for key in State.__dict__.keys():
-            if key is l_method:
-                doc = key.__doc__
-                self.assertIsNotNone(doc)
+from models import State
+import datetime
 
 
 class TestState(unittest.TestCase):
-    """ Test creation objects and use methods """
 
-    @classmethod
-    def setUpClass(cls):
-        ''' new_state up '''
-        cls_state = State()
-        cls_state.name = "Tunis"
-        cls_state.save()
-        cls_state_str = cls_state.to_dict()
+    def setUp(self):
+    
+        self.test_model1 = State()
+        self.test_model2 = State()
 
-    def test_create_object(self):
-        """ Test created instance """
-        new_state = State()
-        self.assertIsInstance(new_state, State)
+    def test_basic_setup(self):
+    
+        self.assertTrue(hasattr(self.test_model1, "name"))
+        self.assertFalse(hasattr(self.test_model1, "first_name"))
+        self.assertTrue(self.test_model1.id != self.test_model2.id)
+        m1c = self.test_model1.created_at
+        m2c = self.test_model2.created_at
+        self.assertTrue(m1c != m2c)
+        self.assertTrue(type(m1c) is datetime.datetime)
 
-    def test_method_save(self):
-        """ Test save method """
-        new_state = State()
-        current = new_state.updated_at
-        new_state.save()
-        new = new_state.updated_at
-        self.assertNotEqual(current, new)
+    def test_types(self):
+    
+        self.assertTrue(type(self.test_model1.name) is str)
 
-    def test_hasMethods(self):
-        """test the instance have the methods"""
-        new_state = State()
-        self.assertTrue(hasattr(new_state, '__str__'))
-        self.assertTrue(hasattr(new_state, '__init__'))
-        self.assertTrue(hasattr(new_state, 'to_dict'))
-        self.assertTrue(hasattr(new_state, 'save'))
-
-    def test_add_attributes(self):
-        """ add attributes to object"""
-        new_state = State()
-        new_state.name = "Tunis"
-        list = [new_state.name]
-        expected = ["Tunis"]
-        self.assertEqual(expected, list)
-
+    def test_save(self):
+    
+        m1u = self.test_model1.updated_at
+        self.test_model1.save()
+        m1u_saved = self.test_model1.updated_at
+        self.assertFalse(m1u == m1u_saved)
 
 if __name__ == '__main__':
     unittest.main()
